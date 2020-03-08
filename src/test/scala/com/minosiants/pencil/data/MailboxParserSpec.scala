9f3d32f550e8a1ab21799fda1235984e715b0d2f
Kaspar Minosiants <kaspar.ru@gmail.com>
2020-03-08 17:32:00 +0300

    Add spec for MailboxParser

---
+++ src/test/scala/com/minosiants/pencil/data/MailboxParserSpec.scala
@@ -0,0 +1,49 @@
+package com.minosiants.pencil.data
+
+import com.minosiants.pencil.data.Error.InvalidMailBox
+import org.specs2.execute.Result
+import org.specs2.mutable.Specification
+
+class MailboxParserSpec extends Specification {
+
+  "MailboxParser" should {
+
+    "parse correctly" in {
+
+      val emails = List(
+        "name.ru@somedomain.com",
+        ".dha134@dom.nab.com",
+        "dha134@dom.nab22.com",
+        "sdfe33d@saf-sf.com"
+      )
+
+      Result.foreach(emails) { e =>
+        val result       = MailboxParser.parse(e)
+        val Array(lp, d) = e.split("@")
+        result must beRight(Mailbox(lp, d))
+      }
+    }
+
+    "parse with error" in {
+
+      val emails = List(
+        "name..ru@somedomain.com",
+        "dha134@-dom.nab22.com",
+        "sdfe33d@saf-sf.com-",
+        "sdfe33d@-saf-sf-.com",
+        "sdfe33d@",
+        "sdfe33d"
+      )
+
+      Result.foreach(emails) { e =>
+        val result = MailboxParser.parse(e)
+        result match {
+          case Left(InvalidMailBox(_)) =>
+            success
+          case _ =>
+            failure(s"$e has not been parsed correctly ")
+        }
+      }
+    }
+  }
+}
