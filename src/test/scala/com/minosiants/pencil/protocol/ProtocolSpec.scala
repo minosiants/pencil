9b737ef953af509a14685fcbccee5759e75b32d1
Kaspar Minosiants <kaspar.ru@gmail.com>
2020-03-31 00:04:07 +0300

    Add scalacheck for protocol testing

--- src/test/scala/com/minosiants/pencil/protocol/ProtocolSpec.scala
+++ src/test/scala/com/minosiants/pencil/protocol/ProtocolSpec.scala
@@ -1,16 +1,17 @@
 package com.minosiants.pencil
 package protocol
 
-import com.minosiants.pencil.data.Mailbox
-import com.minosiants.pencil.protocol.Command._
+import org.scalacheck.Prop
+import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
-import scodec.DecodeResult
+
+import scodec.{ Attempt, DecodeResult }
 import scodec.bits._
 import scodec.codecs._
 
 import scala.io.Source
 
-class ProtocolSpec extends Specification {
+class ProtocolSpec extends Specification with ScalaCheck with ProtocolGens {
 
   "Protocol" should {
 
@@ -23,61 +24,22 @@ class ProtocolSpec extends Specification {
       result.toEither must beRight(DecodeResult(expected, BitVector.empty))
     }
 
-    "decode reply" in {
-      val result = Reply.replyCodec.decode("250-PIPELINING".toBitVector)
-      result.toEither must beRight(
-        DecodeResult(
-          Reply(Code.code(250).get, "-", "PIPELINING"),
-          BitVector.empty
-        )
-      )
+    "reply encoding" in Prop.forAll(replyGen) { reply =>
+      val encoded = Reply.replyCodec.encode(reply)
+      val decoded = encoded.flatMap(Reply.replyCodec.decode)
+      decoded ==== Attempt.successful(DecodeResult(reply, CRLF))
     }
 
-    "decode EHLO command" in {
-      val result = Command.codec.decode(command("EHLO domain \r\n")).toEither
-      result must beRight(DecodeResult(Ehlo("domain"), BitVector.empty))
+    "mailbox encoding" in Prop.forAll(mailboxGen) { box =>
+      val encoded = MailboxCodec.codec.encode(box)
+      val decoded = encoded.flatMap(MailboxCodec.codec.decode)
+      decoded ==== Attempt.successful(DecodeResult(box, BitVector.empty))
     }
 
-    "decode MAIL command" in {
-      val box = "name@domain.com"
-      val result =
-        Command.codec.decode(command(s"MAIL FROM: <$box> \r\n")).toEither
-      result must beRight(
-        DecodeResult(Mail(Mailbox.unsafeFromString(box)), BitVector.empty)
-      )
+    "command encoding" in Prop.forAll(commandGen) { command =>
+      val encoded = Command.codec.encode(command)
+      val decoded = encoded.flatMap(Command.codec.decode)
+      decoded ==== Attempt.successful(DecodeResult(command, BitVector.empty))
     }
-
-    "decode RCPT command" in {
-      val box = "name2@domain2.com"
-      val result =
-        Command.codec.decode(command(s"RCPT TO: <$box> \r\n")).toEither
-      result must beRight(
-        DecodeResult(Rcpt(Mailbox.unsafeFromString(box)), BitVector.empty)
-      )
-    }
-
-    "decode DATA command" in {
-      val result = Command.codec.decode(command(s"DATA: \r\n")).toEither
-      result must beRight(DecodeResult(Data, BitVector.empty))
-    }
-
-    "decode QUIT command" in {
-      val result = Command.codec.decode(command(s"QUIT: \r\n")).toEither
-      result must beRight(DecodeResult(Quit, BitVector.empty))
-    }
-    "encode/decode replies" in {
-
-      val replies = DataSamples.ehloReplies
-
-      val result = Reply.repliesCodec.encode(replies).toEither.flatMap { bits =>
-        Reply.repliesCodec.decode(bits).toEither
-      }
-      println(result)
-      result must beRight(DecodeResult(replies, BitVector.empty))
-    }
-  }
-
-  def command(str: String): BitVector = {
-    ascii.encode(str) getOrElse (BitVector.empty)
   }
 }
