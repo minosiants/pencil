77dcc3796865c02632eabf4aa40e8ae11bbd4372
Ingar Abrahamsen <ingar@abrahams1.com>
2023-02-11 11:51:31 +0100

    feat: reimplement macros for scala 3

--- src/main/scala-3/com/minosiants/pencil/syntax/LiteralsSyntax.scala
+++ src/main/scala-3/com/minosiants/pencil/syntax/LiteralsSyntax.scala
@@ -20,7 +20,51 @@ import java.nio.file.Paths
 import com.minosiants.pencil.data._
 import org.typelevel.literally.Literally
 
-
 trait LiteralsSyntax {
+  extension (inline sc: StringContext)
+    inline def mailbox(args: Any*): Mailbox = ${ LiteralsOps.MailboxLiteral('sc, 'args) }
+    inline def to(args: Any*): To = ${ LiteralsOps.ToLiteral('sc, 'args) }
+    inline def from(args: Any*): From = ${ LiteralsOps.FromLiteral('sc, 'args) }
+    inline def cc(args: Any*): Cc = ${ LiteralsOps.CcLiteral('sc, 'args) }
+    inline def bcc(args: Any*): Bcc = ${ LiteralsOps.BccLiteral('sc, 'args) }
+    inline def subject(): Subject = Subject(sc.s())
+    inline def attachment(): Attachment = Attachment(Paths.get(sc.s()))
+}
+
+object LiteralsOps {
+  object MailboxLiteral extends Literally[Mailbox] {
+    def validate(s: String)(using Quotes) =
+      Mailbox.fromString(s) match
+          case Left(value) => Left(value.getMessage)
+          case Right(_) => Right('{ Mailbox.unsafeFromString(${ Expr(s) }) })
+  }
+
+  object ToLiteral extends Literally[To] {
+    def validate(s: String)(using Quotes) =
+      Mailbox.fromString(s) match
+        case Left(value) => Left(value.getMessage)
+        case Right(_) => Right('{To(Mailbox.unsafeFromString(${ Expr(s) })) })
+  }
+
+  object FromLiteral extends Literally[From] {
+    def validate(s: String)(using Quotes) =
+      Mailbox.fromString(s) match
+        case Left(value) => Left(value.getMessage)
+        case Right(_) => Right('{ From(Mailbox.unsafeFromString(${ Expr(s) })) })
+  }
+
+  object CcLiteral extends Literally[Cc] {
+    def validate(s: String)(using Quotes) =
+      Mailbox.fromString(s) match
+        case Left(value) => Left(value.getMessage)
+        case Right(_) => Right('{ Cc(Mailbox.unsafeFromString(${ Expr(s) })) })
+  }
+
+  object BccLiteral extends Literally[Bcc] {
+    def validate(s: String)(using Quotes) =
+      Mailbox.fromString(s) match
+        case Left(value) => Left(value.getMessage)
+        case Right(_) => Right('{ Bcc(Mailbox.unsafeFromString(${ Expr(s) })) })
+  }
 
 }
