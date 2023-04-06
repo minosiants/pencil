44c91b34466217d1632d0986165e07a6f24fe709
Kaspar Minosiants <kaspar.ru@gmail.com>
2023-04-06 14:47:19 +1000

    Refactor codec

--- src/test/scala/com/minosiants/pencil/DataSamples.scala
+++ src/test/scala/com/minosiants/pencil/DataSamples.scala
@@ -4,7 +4,7 @@ import protocol._
 
 object DataSamples {
 
-  val `220 Greeting` = Replies(
+  case `220 Greeting` = Replies(
     List(
       Reply(
         Code.code(220).get,
@@ -14,23 +14,23 @@ object DataSamples {
     )
   )
 
-  val `mail.example.com` = Reply(Code.code(250).get, "-", "mail.example.com")
+  case `mail.example.com` = Reply(Code.code(250).get, "-", "mail.example.com")
   val PIPELINING: Reply  = Reply(Code.code(250).get, "-", "PIPELINING")
-  val `8BITMIME`: Reply  = Reply(Code.code(250).get, " ", "8BITMIME")
+  case `8BITMIME`: Reply  = Reply(Code.code(250).get, " ", "8BITMIME")
 
   val ehloReplies: Replies = Replies(
     List(`mail.example.com`, PIPELINING, `8BITMIME`)
   )
 
-  val `250 OK` = Replies(
+  case `250 OK` = Replies(
     Reply(Code.code(250).get, " ", "2.1.0 Ok")
   )
 
-  val `221 Buy` = Replies(
+  case `221 Buy` = Replies(
     Reply(Code.code(221).get, " ", "2.0.0 Bye")
   )
 
-  val `354 End data` = Replies(
+  case `354 End data` = Replies(
     Reply(Code.code(354).get, " ", "End data with <CR><LF>.<CR><LF>")
   )
 
