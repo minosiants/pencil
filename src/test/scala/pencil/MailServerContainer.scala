3131b20332a7f117470eb2a344b4117aa4711ff3
Kaspar Minosiants <kaspar.ru@gmail.com>
2023-06-14 11:42:04 +1000

    Add test for send email

--- src/test/scala/pencil/MailServerContainer.scala
+++ src/test/scala/pencil/MailServerContainer.scala
@@ -6,13 +6,15 @@ import org.testcontainers.containers.wait.strategy.{LogMessageWaitStrategy, Wait
 import org.testcontainers.utility.DockerImageName
 import pencil.data.Credentials
 import cats.syntax.show.*
+
+import java.nio.file.Paths
 import sys.process.*
 import scala.jdk.CollectionConverters.*
 trait MailServerContainer:
-  def foo(): String
   def start(): Unit
   def stop(): Unit
   def smtpPort:Int
+  def httpPort:Int
   def socketAddress() = SocketAddress(host"localhost", Port.fromInt(smtpPort).get)
 
   def credentials:Credentials
@@ -20,29 +22,27 @@ object MailServerContainer:
   def mk() =
     val username = Username("pencil")
     val password = Password("pencil1234")
-    val container = GenericContainer(DockerImageName.parse("juanluisbaptiste/postfix:latest"))
-      container.addEnv("SMTP_SERVER","mail.pencil.com")
-      container.addEnv("SMTP_USERNAME",username.show)
-      container.addEnv("SMTP_PASSWORD", password.show)
-      container.addEnv("SERVER_HOSTNAME", "mail.pencil.com")
-      container.addExposedPorts(25,25)
-     // container.addExposedPorts(587, 587)
-      //container.withNetwork()
-     //container.addEnv("smtp_user", "pencil:pencil1234")
-   //  container.withClasspathResourceMapping("certs", "/etc/postfix/certs", BindMode.READ_ONLY)
+    val smtp = 1025
+    val http = 8025
 
+    val container = GenericContainer(DockerImageName.parse("axllent/mailpit"))
+    container.withClasspathResourceMapping("certs", "/data", BindMode.READ_ONLY)
+    container.addExposedPorts(smtp, http)
+    //container.addExposedPorts(httpPort, httpPort)
+    container.addEnv("MP_SMTP_AUTH_FILE","/data/pass.txt")
+    container.addEnv("MP_SMTP_TLS_CERT","/data/certificate.crt")
+    container.addEnv("MP_SMTP_TLS_KEY", "/data/keyfile.key")
+    container.addEnv("MP_SMTP_AUTH_ALLOW_INSECURE", "true")
 
-    //container.waitingFor(Wait.forLogMessage("mail.pencil.com is up and running", 1))
     new MailServerContainer:
       override def start(): Unit =
         container.start()
-        container.waitingFor(Wait.forLogMessage("postfix entered RUNNING state", 1))
-
+        container.waitingFor(Wait.forListeningPort())
 
       override def stop(): Unit = container.stop()
 
-      override def foo(): String = ""
-
       override def credentials: Credentials = Credentials(username, password)
 
-      override def smtpPort = container.getMappedPort(25)
+      override def httpPort: Int = container.getMappedPort(http)
+
+      override def smtpPort: Int = container.getMappedPort(smtp)
--- src/test/scala/pencil/MailServerSpec.scala
+++ src/test/scala/pencil/MailServerSpec.scala
@@ -14,7 +14,7 @@ import pencil.syntax.*
 import cats.syntax.show.*
 import cats.syntax.flatMap.*
 import pencil.protocol.{Replies, Code}
-
+import cats.syntax.either.*
 class MailServerSpec extends SpecificationLike with LiteralsSyntax  with BeforeAll with AfterAll:
   val logger = Slf4jLogger.getLogger[IO]
   val container = MailServerContainer.mk()
@@ -25,10 +25,21 @@ class MailServerSpec extends SpecificationLike with LiteralsSyntax  with BeforeA
 
   def runC[R](command: Smtp[IO, R])(using email:Email):IO[R] =
     Network[IO].client(container.socketAddress()).use { s =>
-      (Smtp.rset[IO]() >> command).run(Request(email, SmtpSocket.fromSocket[IO](s, logger)))
+      (/*Smtp.rset[IO]() >>*/ command).run(Request(email, SmtpSocket.fromSocket[IO](s, logger)))
     }
 
+  def printError:PartialFunction[Throwable, Throwable] = {
+    case e: pencil.data.Error =>
+      println(s"error: ${e.show}")
+      e
+    case e: Throwable =>
+      e.printStackTrace()
+      e
+
+  } 
   extension [R](c:Smtp[IO, R])
     def runCommand(using email:Email): R = runC(c).unsafeRunSync()
+    def attempt(using email: Email): Either[Throwable, R] =
+      runC(c).attempt.unsafeRunSync().leftMap(printError)
 
 
